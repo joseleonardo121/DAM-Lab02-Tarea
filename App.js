08440447cd7c69e2bd645b68498ac570f71af1c2
joseleonardo123 <jose.gonzales.p@tecsup.edu.pe>
2022-03-31 23:20:21 -0500

    v1.1

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View,TextInput,Image,TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    const [count, setCount] = useState(0);
+    const onPress = () => setCount(prevCount => prevCount + 1);
+  
+    return (
+      <View style={styles.container}>
+      <Image source={require('./img/origen.png')}/>
+      <Text>Usuario:</Text>
+      <TextInput style={styles.input}
+      placeholder='Usuario'
+      onChangeText={(val)=>setUsuario(val)}
+      />
+
+      <Text>Contraseña:</Text>
+      <TextInput style={styles.input}
+      placeholder='Contraseña'
+      onChangeText={(val)=>setContraseña(val)}
+      />
+    <View style={styles.countContainer}>
+        <Text>Count: {count}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+
+      
+
+
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +46,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:200,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  }
 });
